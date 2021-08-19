46fa68523082390dd4713a9460dc59c75bf52850
andrei-brando <albertbrando@hotmail.com>
2021-08-19 08:44:39 -0300

    adiciona mais testes

--- tests/features/notes/presentation/controllers/notes.controller.spec.ts
+++ tests/features/notes/presentation/controllers/notes.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Note } from "../../../../../src/features/notes/domain/models";
 import { CacheRepository, NoteRepository } from "../../../../../src/features/notes/infra";
-import { HttpRequest, NoteController, ok } from "../../../../../src/features/notes/presentation";
+import { HttpRequest, NoteController, notFound, ok, serverError } from "../../../../../src/features/notes/presentation";
 
 jest.mock('../../../../../src/features/notes/infra/repositories/note.repository.ts');
 jest.mock('../../../../../src/core/infra/repositories/cache.repository.ts');
@@ -25,6 +25,15 @@ const makeRequestBody = (): HttpRequest => {
   }
 }
 
+const makeRequestParams = (): HttpRequest => {
+  return {
+    body: {},
+    params: {
+      userUid: 'any_uid',
+    }
+  }
+}
+
 const makeSut = (): NoteController => {
   return new NoteController(
     new NoteRepository(),
@@ -37,6 +46,82 @@ describe('NOTES CONTROLLER', () => {
     jest.clearAllMocks();
   });
 
+  describe('INDEX', () => {
+    test('should return code 500 when throw any exception', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue(null);
+      jest.spyOn(NoteRepository.prototype, 'getAll')
+        .mockRejectedValue(new Error());
+
+      const sut = makeSut();
+      const result = await sut.index(makeRequestParams());
+
+      expect(result).toEqual(serverError());
+    });
+
+    test('should return code 200 when get notes on cache', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue([makeResult]);
+
+      const sut = makeSut();
+      const result = await sut.index(makeRequestParams());
+
+      expect(result).toEqual(ok([makeResult]));
+    });
+
+    test('should return code 404 when not exists notes', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue(null);
+      jest.spyOn(NoteRepository.prototype, 'getAll')
+        .mockResolvedValue([]);
+
+      const sut = makeSut();
+      const result = await sut.index(makeRequestParams());
+
+      expect(result).toEqual(notFound());
+    });
+
+    test('should call and set cache repository', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue(null);
+      jest.spyOn(NoteRepository.prototype, 'getAll')
+        .mockResolvedValue([makeResult()]);
+
+      const setSpy = jest.spyOn(CacheRepository.prototype, 'set')
+        .mockResolvedValue(null);
+
+      const sut = makeSut();
+      const result = await sut.index(makeRequestParams());
+
+      expect(setSpy).toHaveBeenCalledWith(`notes:all:user:any_uid`, [makeResult()]);
+      expect(result).toEqual(ok([makeResult()]))
+    });
+  });
+
+  describe('SHOW', () => {
+    test('should return code 500 when throw any exception', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue(null);
+      jest.spyOn(NoteRepository.prototype, 'getOne')
+        .mockRejectedValue(new Error());
+
+      const sut = makeSut();
+      const result = await sut.show(makeRequestParams());
+
+      expect(result).toEqual(serverError());
+    });
+
+    test('should return code 200 when get note on cache', async () => {
+      jest.spyOn(CacheRepository.prototype, 'get')
+        .mockResolvedValue(makeResult());
+
+      const sut = makeSut();
+      const result = await sut.show(makeRequestParams());
+
+      expect(result).toEqual(ok(makeResult()));
+    });
+  });
+
   describe('STORE', () => {
     test('should return code 200 when valid data is provided', async () => {
       jest.spyOn(NoteRepository.prototype, 'create')
@@ -46,6 +131,16 @@ describe('NOTES CONTROLLER', () => {
       const result = await sut.store(makeRequestBody());
 
       expect(result).toEqual(ok(makeResult()));
+    });
+
+    test('should return code 500 when throw any exception', async () => {
+      jest.spyOn(NoteRepository.prototype, 'create')
+        .mockRejectedValue(new Error());
+
+      const sut = makeSut();
+      const result = await sut.store(makeRequestBody());
+
+      expect(result).toEqual(serverError());
     });
   });
 });
