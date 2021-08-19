188d77e7c905a0eb166f594e1e5453848748ebcb
andrei-brando <albertbrando@hotmail.com>
2021-08-19 00:07:39 -0300

    ajuste no ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -4,27 +4,42 @@ const rootPath = process.env.NODE_ENV?.toLocaleLowerCase() === 'production'
   ? 'dist'
   : 'src';
 
-console.log(rootPath);
+let config = {};
 
-module.exports = {
-  type: process.env.DIALECT,
-  // port: process.env.PORT,
-  url: process.env.DATABASE_URL,
-  synchronize: false,
-  logging: false,
-  entities: [
-    `${rootPath}/core/infra/data/database/entities/**/*`
-  ],
-  migrations: [
-    `${rootPath}/core/infra/data/database/migrations/**/*`,
-  ],
-  cli: {
-    entitiesDir: `${rootPath}/core/infra/data/database/entities`,
-    migrationsDir: `${rootPath}/core/infra/data/database/migrations`,
-  },
-  extra: {
-    ssl: {
-      rejectUnauthorized: false
+if (process.env.NODE_ENV?.toLocaleLowerCase() === 'test') {
+  config = {
+    type: 'sqlite',
+    database: './testedb.sql',
+    entities: [
+      'src/core/infra/data/database/entities/**/*',
+    ],
+    migrations: [
+      'src/core/infra/data/database/migrations/**/*',
+    ]
+  }
+} else {
+  config = {
+    type: process.env.DIALECT,
+    // port: process.env.PORT,
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    entities: [
+      `${rootPath}/core/infra/data/database/entities/**/*`
+    ],
+    migrations: [
+      `${rootPath}/core/infra/data/database/migrations/**/*`,
+    ],
+    cli: {
+      entitiesDir: `${rootPath}/core/infra/data/database/entities`,
+      migrationsDir: `${rootPath}/core/infra/data/database/migrations`,
+    },
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   }
-};
+}
+
+module.exports = config;
